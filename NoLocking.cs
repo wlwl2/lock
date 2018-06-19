313443a1eba11f9d6371f4d4ee83299b367d129c
William Lim <manvinedi@gmail.com>
2018-06-19 13:12:41 +0800

    first commit

---
+++ NoLocking.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace lock
+{
+    public class NoLocking
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
